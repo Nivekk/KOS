011c0f41722a0c34f6d769029f1bc3f04525ee7c
Fabian Neundorf <CommodoreFabianus@gmx.de>
2013-10-12 13:37:33 +0200

    !make the velocity class a vector, being the orbit vector by default;

--- VesselVelocity.cs
+++ VesselVelocity.cs
@@ -5,33 +5,26 @@ using System.Text;
 
 namespace kOS
 {
-    public class VesselVelocity : SpecialValue
+    public class VesselVelocity : Vector
     {
-        Vector orbitVelocity;
         Vector surfaceVelocity;
         float velocityHeading;
 
-        public VesselVelocity(Vessel v)
+        public VesselVelocity(Vessel v) : base(v.obt_velocity)
         {
-            orbitVelocity = new Vector(v.obt_velocity);
             surfaceVelocity = new Vector(v.srf_velocity);
             velocityHeading = VesselUtils.GetVelocityHeading(v);
         }
 
         public override object GetSuffix(string suffixName)
         {
-            if (suffixName == "ORBIT") return orbitVelocity;
+            if (suffixName == "ORBIT") return this;
             if (suffixName == "SURFACE") return surfaceVelocity;
 
             // I created this one for debugging purposes only, at some point I'll make a function to transform vectors to headings in a more eloquent way
             if (suffixName == "SURFACEHEADING") return velocityHeading;
 
             return base.GetSuffix(suffixName);
-        }
-
-        public override string ToString()
-        {
-            return orbitVelocity.ToString();
         }
     }
 }
