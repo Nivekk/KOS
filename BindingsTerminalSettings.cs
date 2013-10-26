817cdc4e5ceb5b5f1f38464eda99036c9de05b17
raad287 <raad287@gmail.com>
2013-10-26 01:13:23 -0700

    -Added volume:selected and volume:freespace

--- BindingsTerminalSettings.cs
+++ BindingsTerminalSettings.cs
@@ -11,6 +11,15 @@ namespace kOS
         public override void AddTo(BindingManager manager)
         {
             manager.AddGetter("SESSIONTIME", delegate(CPU cpu) { return cpu.SessionTime; });
+
+            manager.AddGetter("VOLUME:SELECTED", delegate(CPU cpu)
+            {
+                // if the user hasn't selected another volume, name will remain empty
+                // return 1, as the user will still be on the default volume, 1
+                if (cpu.SelectedVolume.Name == null || cpu.SelectedVolume.Name.Length == 0) { return "1"; }
+                else { return cpu.SelectedVolume.Name; }
+            });
+            manager.AddGetter("VOLUME:FREESPACE", delegate(CPU cpu) { return cpu.SelectedVolume.GetFreeSpace(); });
         }
     }
 }
