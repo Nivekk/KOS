8ebcc0a8df11c73a5606b47fdb48a0e081b56580
Eric Oden <oden.eric@gmail.com>
2013-10-26 17:02:40 -0500

    More formatiing

--- CommandFileIO.cs
+++ CommandFileIO.cs
@@ -197,29 +197,29 @@ namespace kOS
 
         public override void Evaluate()
         {
-        // Todo: let the user specify a volume "LOG something TO file ON volume"
-        Volume targetVolume = SelectedVolume;
+            // Todo: let the user specify a volume "LOG something TO file ON volume"
+            Volume targetVolume = SelectedVolume;
 
-        // If the archive is out of reach, the signal is lost in space.
-        if (!targetVolume.CheckRange())
-        {
-            State = ExecutionState.DONE;
-            return;
-        }
+            // If the archive is out of reach, the signal is lost in space.
+            if (!targetVolume.CheckRange())
+            {
+                State = ExecutionState.DONE;
+                return;
+            }
 
-        String targetFile = RegexMatch.Groups[2].Value.Trim();
-        Expression e = new Expression(RegexMatch.Groups[1].Value, ParentContext);
+            String targetFile = RegexMatch.Groups[2].Value.Trim();
+            Expression e = new Expression(RegexMatch.Groups[1].Value, ParentContext);
 
-        if (e.IsNull())
-        {
-            State = ExecutionState.DONE;
-        }
-        else
-        {
-            targetVolume.AppendToFile(targetFile, e.ToString());
-            State = ExecutionState.DONE;
-        }
-        }
+            if (e.IsNull())
+            {
+                State = ExecutionState.DONE;
+            }
+            else
+            {
+                targetVolume.AppendToFile(targetFile, e.ToString());
+                State = ExecutionState.DONE;
+            }
+         }
     }
     
     [CommandAttribute("COPY &[TO,FROM][VOLUME]? ^")]
